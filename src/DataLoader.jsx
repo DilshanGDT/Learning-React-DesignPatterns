3989ead202af726ca52108504d58d81f53bf0450
DilshanGDT <tharakadilshan506@gmail.com>
2025-10-03 12:02:23 +0530

    exercise : get data from local storage

--- src/DataLoader.jsx
+++ src/DataLoader.jsx
@@ -11,6 +11,16 @@ export const DataLoader = ({ getDataFn = () => {}, resourceName, children }) => 
     })()
   }, []);
 
+//exercise : retrive data from local storage using the key "appData"
+//   useEffect(() => {
+//     (async () => {
+//       const storedData = localStorage.getItem('appData');
+//       if (storedData) {
+//         setData(JSON.parse(storedData));
+//       }
+//     })()
+//   }, []);
+
   return data && React.Children.map(children, child => {
     if (React.isValidElement(child)) {
       return React.cloneElement(child, { [resourceName]: data });
