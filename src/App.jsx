9e1159c183ee95be3d6b9b4083bfc123a020f6ae
DilshanGDT <tharakadilshan506@gmail.com>
2025-10-06 16:01:46 +0530

    printing props with Higher order components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { printProps } from "./printProps"
+import { UserInfo } from "./UserInfo"
 
+const UserInfoWrapped = printProps(UserInfo);
 
 function App() {
   return (
-    <>
-	<h1>React</h1>
-    </>
+    <UserInfoWrapped user={{ name: 'Shaun', age: 123, hairColor: 'brown', hobbies: [] }} />
   )
 }
 
