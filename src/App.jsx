f724bae1daaa2529af2a39a711dc5641458f770d
DilshanGDT <tharakadilshan506@gmail.com>
2025-10-06 16:13:02 +0530

    Loading data with higher order components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { printProps } from "./printProps"
 import { UserInfo } from "./UserInfo"
+import { withUser } from "./withUser";
 
-const UserInfoWrapped = printProps(UserInfo);
+const UserInfoWrapped = withUser(printProps(UserInfo), '123');
 
 function App() {
   return (
-    <UserInfoWrapped user={{ name: 'Shaun', age: 123, hairColor: 'brown', hobbies: [] }} />
+    <UserInfoWrapped />
   )
 }
 
