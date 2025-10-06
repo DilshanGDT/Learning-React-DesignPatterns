d81583e0b235400a1deecaa79c22538c1ec65ea6
DilshanGDT <tharakadilshan506@gmail.com>
2025-10-06 19:58:17 +0530

    6.2 recirsive components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { RecursiveComponent } from "./RecursiveComponent"
 
+const nestedObject = {
+	a: 1,
+	b: {
+		b1: 4,
+		b2: {
+			b23: 'Hello',
+		},
+		b3: {
+			b31: {
+				message: 'Hi',
+			},
+			b32: {
+				message: 'Hi',
+			}
+		}
+	},
+	c: {
+		c1: 2,
+		c2: 3,
+	}
+}
 
 function App() {
   return (
-    <>
-	<h1>React</h1>
-    </>
+    <RecursiveComponent data={nestedObject} />
   )
 }
 
